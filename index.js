5b91870fb267f02488be6f8ebc70912d5f38b78a
Matheus <matheusfariasdasneves@gmail.com>
2024-03-08 21:01:05 -0300

    feat: add CreateUserController and validations

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import 'dotenv/config.js'
 import express from 'express'
 
+import { CreateUserController } from './src/controllers/create-user.js'
+
 const app = express()
 
 app.use(express.json())
 
-app.listen(process.env.PORT, () => console.log('listening on port 8080'))
+app.post('/api/users', async (req, res) => {
+  const createUserController = new CreateUserController()
+
+  const { statusCode, body } = await createUserController.execute(req)
+
+  res.status(statusCode).json(body)
+})
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`),
+)
---
+++ src/controllers/create-user.js
@@ -0,0 +1,45 @@
+import { CreateUserUseCase } from '../use-case/create-user.js'
+import validator from 'validator'
+
+import { badRequest, created, serverError } from './helpers.js'
+
+export class CreateUserController {
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body
+
+      const requiredFields = ['first_name', 'last_name', 'email', 'password']
+
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing param: ${field}` })
+        }
+      }
+
+      const passwordIsInvalid = params.password.length < 6
+
+      if (passwordIsInvalid) {
+        return badRequest({
+          message: 'Password must have at least 6 characters',
+        })
+      }
+
+      const emailIsValid = validator.isEmail(params.email)
+
+      if (!emailIsValid) {
+        return badRequest({
+          message: 'Invalid e-mail. Please provide a valid one.',
+        })
+      }
+
+      const createUserUseCase = new CreateUserUseCase()
+
+      const createdUser = await createUserUseCase.execute(params)
+
+      return created(createdUser)
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
+  }
+}

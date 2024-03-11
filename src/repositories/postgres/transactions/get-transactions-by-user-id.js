971964bbe19029f51317a3fd51d4c67116010f92
Matheus <matheusfariasdasneves@gmail.com>
2024-03-11 18:29:01 -0300

    feat: add PostgresGetTransactionsByUserIdRepository

---
+++ src/repositories/postgres/transactions/get-transactions-by-user-id.js
@@ -0,0 +1,12 @@
+import { postgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresGetTransactionsByUserIdRepository {
+  async execute(userId) {
+    const result = await postgresHelper.query(
+      'SELECT * FROM transactions WHERE user_id = $1',
+      [userId],
+    )
+
+    return result[0]
+  }
+}

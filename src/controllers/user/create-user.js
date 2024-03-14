e882e15bb49e2d9707e092c1a5df3ffc3024692b
Matheus <matheusfariasdasneves@gmail.com>
2024-03-14 00:59:15 -0300

    feat: add zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,15 +1,7 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-  emailAlreadyInUseResponse,
-  invalidPasswordResponse,
-  checkIfPasswordIsInvalid,
-  checkIfEmailIsInvalid,
-  badRequest,
-  created,
-  serverError,
-  validateRequiredFields,
-  requiredFieldIsMissingResponse,
-} from '../helpers/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
+import { ZodError } from 'zod'
+import { createUserSquema } from '../../schemas/index.js'
 
 export class CreateUserController {
   constructor(createUserUseCase) {
@@ -20,31 +12,18 @@ export class CreateUserController {
     try {
       const params = httpRequest.body
 
-      const requiredFields = ['first_name', 'last_name', 'email', 'password']
-
-      const { ok: requiredFieldWereProvided, missingField } =
-        validateRequiredFields(params, requiredFields)
-
-      if (!requiredFieldWereProvided) {
-        return requiredFieldIsMissingResponse(missingField)
-      }
-
-      const passwordIsInvalid = checkIfPasswordIsInvalid(params.password)
-
-      if (passwordIsInvalid) {
-        return invalidPasswordResponse()
-      }
-
-      const emailIsValid = checkIfEmailIsInvalid(params.email)
-
-      if (!emailIsValid) {
-        return emailAlreadyInUseResponse()
-      }
+      await createUserSquema.parseAsync(params)
 
       const createdUser = await this.createUserUseCase.execute(params)
 
       return created(createdUser)
     } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.errors[0].message,
+        })
+      }
+
       if (error instanceof EmailAlreadyInUseError) {
         return badRequest({ message: error.message })
       }
---
+++ src/schemas/user.js
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+export const createUserSquema = z.object({
+  first_name: z
+    .string({
+      required_error: 'First name is required.',
+    })
+    .trim()
+    .min(1, {
+      message: 'First name is required.',
+    }),
+  last_name: z
+    .string({
+      required_error: 'Last name is required.',
+    })
+    .trim()
+    .min(1, {
+      message: 'Last name is required.',
+    }),
+  email: z
+    .string({
+      required_error: 'Email is required.',
+    })
+    .email({
+      message: 'Please provide a valid email address.',
+    })
+    .trim()
+    .min(1, {
+      message: 'Email is required.',
+    }),
+  password: z
+    .string({
+      required_error: 'Password is required.',
+    })
+    .trim()
+    .min(6, {
+      message: 'Password must have at least 6 characters',
+    }),
+})

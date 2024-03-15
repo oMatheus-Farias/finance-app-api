7c6d23b8f6d0697e0170e9df23ff7c4af48d87a1
Matheus <matheusfariasdasneves@gmail.com>
2024-03-15 16:11:18 -0300

    feat: ensure GetUserBalanceRepository uses Prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,25 +1,54 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
   async execute(userId) {
-    const result = await postgresHelper.query(
-      `SELECT
-        SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-        SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-        SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investiments,
-        (
-          SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) 
-          - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
-          - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-        ) AS balance
-        FROM transactions
-        WHERE user_id = $1;`,
-      [userId],
-    )
+    const {
+      _sum: { amount: totalExpenses },
+    } = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EXPENSE',
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+
+    const {
+      _sum: { amount: totalEarnings },
+    } = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EARNING',
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+
+    const {
+      _sum: { amount: totalInvestments },
+    } = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'INVESTMENT',
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+
+    const _totalExpenses = totalExpenses || 0
+    const _totalEarnings = totalEarnings || 0
+    const _totalInvestments = totalInvestments || 0
+
+    const balance = _totalEarnings - _totalExpenses - _totalInvestments
 
     return {
-      userId,
-      ...result[0],
+      expenses: _totalExpenses,
+      earnings: _totalEarnings,
+      investments: _totalInvestments,
+      balance,
     }
   }
 }

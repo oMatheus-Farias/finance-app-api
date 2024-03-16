638ab1d11cf0aea66ff98a5530474fc95da64338
Matheus <matheusfariasdasneves@gmail.com>
2024-03-16 19:26:34 -0300

    test: ensure 400 is returned if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -99,4 +99,22 @@ describe('Create User Controller', () => {
 
     expect(result.statusCode).toBe(400)
   })
+
+  it('should return 400 if password is not provided', async () => {
+    const craeteUserUseCase = new CreateUserUseCaseStub()
+
+    const createUserController = new CreateUserController(craeteUserUseCase)
+
+    const httpRequest = {
+      body: {
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john@doe.com',
+      },
+    }
+
+    const result = await createUserController.execute(httpRequest)
+
+    expect(result.statusCode).toBe(400)
+  })
 })

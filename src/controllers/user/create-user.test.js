aeb4f63c44226d4b989a554568489efa63175ba2
Matheus <matheusfariasdasneves@gmail.com>
2024-03-16 19:25:13 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -80,4 +80,23 @@ describe('Create User Controller', () => {
 
     expect(result.statusCode).toBe(400)
   })
+
+  it('should return 400 if email is not valid', async () => {
+    const createUserUseCase = new CreateUserUseCaseStub()
+
+    const createUserController = new CreateUserController(createUserUseCase)
+
+    const httpRequest = {
+      body: {
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john',
+        password: 'password',
+      },
+    }
+
+    const result = await createUserController.execute(httpRequest)
+
+    expect(result.statusCode).toBe(400)
+  })
 })

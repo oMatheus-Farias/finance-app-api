963b662aaf6b7038b328bb73857c9ab4d409389d
Matheus <matheusfariasdasneves@gmail.com>
2024-03-17 23:03:03 -0300

    test: ensure DeleteTransactionController returns 404 when transaction is not found

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -47,4 +47,15 @@ describe('Delete Transaction Controller', () => {
 
     expect(result.statusCode).toBe(400)
   })
+
+  it('should return 404 when transaction is not found', async () => {
+    const { deleteTransactionController, sut } = makeSut()
+    jest
+      .spyOn(deleteTransactionController, 'execute')
+      .mockResolvedValueOnce(null)
+
+    const result = await sut.execute(httpRequest)
+
+    expect(result.statusCode).toBe(404)
+  })
 })

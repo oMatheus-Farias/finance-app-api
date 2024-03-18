a96ecd5d6927ce0ccde88a98c201fcc12e3a3bb8
Matheus <matheusfariasdasneves@gmail.com>
2024-03-18 15:39:51 -0300

    refactor: remove unused code

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,29 +1,4 @@
-import validator from 'validator'
-
-import { badRequest, notFound } from './index.js'
-
-export const checkIfAmountIsvalid = (amount) => {
-  if (typeof amount !== 'number') {
-    return false
-  }
-
-  return validator.isCurrency(amount.toFixed(2), {
-    digits_after_decimal: [2],
-    allow_negatives: false,
-    decimal_separator: '.',
-  })
-}
-
-export const checkIfTypeIsValid = (type) =>
-  ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type)
-
-export const invalidAmoundResponse = () =>
-  badRequest({ message: `The amount must a be a valid currency.` })
-
-export const invalidTypeResponse = () =>
-  badRequest({
-    message: `The type must be EARNING, EXPENSE or INVESTMENT.`,
-  })
+import { notFound } from './index.js'
 
 export const transactionNotFoundResponse = () =>
   notFound({ message: `Transaction not found.` })
--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,14 +1,4 @@
-import { badRequest, notFound } from './index.js'
-
-export const invalidPasswordResponse = () =>
-  badRequest({
-    message: 'Password must be at least 6 characters.',
-  })
-
-export const emailAlreadyInUseResponse = () =>
-  badRequest({
-    message: 'Invalid e-mail. Please provide a valid one.',
-  })
+import { notFound } from './index.js'
 
 export const userNotFoundResponse = () =>
   notFound({ message: 'User not found.' })

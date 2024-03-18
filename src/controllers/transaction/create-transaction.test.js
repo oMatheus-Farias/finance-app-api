44b29ac1ec1fff86ee46d026ce6fa1e84a9f609b
Matheus <matheusfariasdasneves@gmail.com>
2024-03-17 22:34:34 -0300

    test: ensure CreateTransactionController returns 500 if CreateTransactionUseCase throws

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -123,4 +123,15 @@ describe('Create Transaction Controller', () => {
 
     expect(result.statusCode).toBe(400)
   })
+
+  it('should return 500 if CreateTransactionUseCase throws', async () => {
+    const { createTransactionUseCase, sut } = makeSut()
+    jest
+      .spyOn(createTransactionUseCase, 'execute')
+      .mockRejectedValueOnce(new Error())
+
+    const result = await sut.execute(httpRequest)
+
+    expect(result.statusCode).toBe(500)
+  })
 })

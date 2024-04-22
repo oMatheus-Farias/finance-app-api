b3201702eec98655843c21e2368cfdf00d2c1122
Matheus <matheusfariasdasneves@gmail.com>
2024-04-21 22:05:39 -0300

    test: ensure PostgresGetUserByIdRepository get user by id a db

---
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+import { PostgresGetUserByIdRepository } from './get-user-by-id.js'
+import { prisma } from '../../../../prisma/prisma.js'
+
+describe('Postgres Get User By Id Repository', () => {
+    const sut = new PostgresGetUserByIdRepository()
+
+    const fakeUser = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 7,
+        }),
+    }
+
+    it('should get user by id a db', async () => {
+        const user = await prisma.user.create({ data: fakeUser })
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toStrictEqual(fakeUser)
+    })
+})

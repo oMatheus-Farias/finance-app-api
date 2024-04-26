3948a727ee4eea212def27099424aee55662d877
Matheus <matheusfariasdasneves@gmail.com>
2024-04-26 19:36:20 -0300

    feat: ensure DeleteUserController return 404 if UserNotFoundError is throw

--- src/controllers/user/delete-user.js
+++ src/controllers/user/delete-user.js
@@ -1,36 +1,37 @@
+import { UserNotFoundError } from '../../errors/user.js'
 import {
-  checkIfIdIsValid,
-  invalidIdResponse,
-  ok,
-  userNotFoundResponse,
-  serverError,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    userNotFoundResponse,
+    serverError,
 } from '../helpers/index.js'
 
 export class DeleteUserController {
-  constructor(deleteUserUseCase) {
-    this.deleteUserUseCase = deleteUserUseCase
-  }
-
-  async execute(httpRequest) {
-    try {
-      const isIdValid = checkIfIdIsValid(httpRequest.params.userId)
-
-      if (!isIdValid) {
-        return invalidIdResponse()
-      }
-
-      const deletedUser = await this.deleteUserUseCase.execute(
-        httpRequest.params.userId,
-      )
-
-      if (!deletedUser) {
-        return userNotFoundResponse()
-      }
-
-      return ok(deletedUser)
-    } catch (error) {
-      console.error(error)
-      return serverError()
+    constructor(deleteUserUseCase) {
+        this.deleteUserUseCase = deleteUserUseCase
     }
-  }
+
+    async execute(httpRequest) {
+        try {
+            const isIdValid = checkIfIdIsValid(httpRequest.params.userId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const deletedUser = await this.deleteUserUseCase.execute(
+                httpRequest.params.userId,
+            )
+
+            return ok(deletedUser)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
 }

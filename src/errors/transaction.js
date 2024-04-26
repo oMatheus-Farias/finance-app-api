697a998da7440438309792c4b3c8eb6371bfb256
Matheus <matheusfariasdasneves@gmail.com>
2024-04-26 18:58:13 -0300

    feat: add TransactionNotFoundError

---
+++ src/errors/transaction.js
@@ -0,0 +1,6 @@
+export class TransactionNotFoundError extends Error {
+    constructor(transactionId) {
+        super(`Transaction with id ${transactionId} was not found`)
+        this.name = 'TransactionNotFoundError'
+    }
+}

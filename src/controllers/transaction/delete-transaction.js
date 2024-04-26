f1cdc5cd27abb4988bbaee1f11fc908cc7f7d9c7
Matheus <matheusfariasdasneves@gmail.com>
2024-04-26 19:17:03 -0300

    feat: ensure DeleteTransactionController returns 404 if TransactionNotFoundError is throw

--- src/controllers/transaction/delete-transaction.js
+++ src/controllers/transaction/delete-transaction.js
@@ -1,36 +1,37 @@
+import { TransactionNotFoundError } from '../../errors/transaction.js'
 import {
-  checkIfIdIsValid,
-  invalidIdResponse,
-  ok,
-  serverError,
-  transactionNotFoundResponse,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+    transactionNotFoundResponse,
 } from '../helpers/index.js'
 
 export class DeleteTransactionController {
-  constructor(deleteTransactionUseCase) {
-    this.deleteTransactionUseCase = deleteTransactionUseCase
-  }
-
-  async execute(httpRequest) {
-    try {
-      const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
-
-      if (!idIsValid) {
-        return invalidIdResponse()
-      }
-
-      const transaction = await this.deleteTransactionUseCase.execute(
-        httpRequest.params.transactionId,
-      )
-
-      if (!transaction) {
-        return transactionNotFoundResponse()
-      }
-
-      return ok(transaction)
-    } catch (error) {
-      console.error(error)
-      return serverError()
+    constructor(deleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase
     }
-  }
+
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const transaction = await this.deleteTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            if (error instanceof TransactionNotFoundError) {
+                return transactionNotFoundResponse()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
 }

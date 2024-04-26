b4f8bdea9d8d90bbe5f579e7291ea8fdd5e7bf9a
Matheus <matheusfariasdasneves@gmail.com>
2024-04-26 18:58:58 -0300

    feat: ensure PostgresDeleteTransactionRepository throws if Prisma throws

--- src/repositories/postgres/transactions/delete-transaction.js
+++ src/repositories/postgres/transactions/delete-transaction.js
@@ -1,15 +1,25 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresDeleteTransactionRepository {
-  async execute(transactionId) {
-    try {
-      return await prisma.transaction.delete({
-        where: {
-          id: transactionId,
-        },
-      })
-    } catch (error) {
-      return null
+    async execute(transactionId) {
+        try {
+            return await prisma.transaction.delete({
+                where: {
+                    id: transactionId,
+                },
+            })
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                const code = error.code
+
+                if (code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+
+            throw error
+        }
     }
-  }
 }

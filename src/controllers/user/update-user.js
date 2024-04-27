fcdeb181bd6c7652ddc9d5673e5c26bb1b682ef7
Matheus <matheusfariasdasneves@gmail.com>
2024-04-27 16:54:54 -0300

    feat: ensure UpdateUserController returns 404 if UserNotFoundError is throw

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,52 +1,60 @@
-import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { EmailAlreadyInUseError, UserNotFoundError } from '../../errors/user.js'
 import { updateUserSquema } from '../../schemas/index.js'
 
 import {
-  invalidIdResponse,
-  checkIfIdIsValid,
-  badRequest,
-  ok,
-  serverError,
+    invalidIdResponse,
+    checkIfIdIsValid,
+    badRequest,
+    ok,
+    serverError,
+    userNotFoundResponse,
 } from '../helpers/index.js'
 import { ZodError } from 'zod'
 
 export class UpdateUserController {
-  constructor(updateUserUseCase) {
-    this.updateUserUseCase = updateUserUseCase
-  }
-
-  async execute(httpRequest) {
-    try {
-      const userId = httpRequest.params.userId
-
-      const isIdValid = checkIfIdIsValid(userId)
-
-      if (!isIdValid) {
-        return invalidIdResponse()
-      }
-
-      const params = httpRequest.body
-
-      await updateUserSquema.parseAsync(params)
-
-      const updatedUser = await this.updateUserUseCase.execute(userId, params)
-
-      return ok(updatedUser)
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return badRequest({
-          message: error.errors[0].message,
-        })
-      }
-
-      if (error instanceof EmailAlreadyInUseError) {
-        return badRequest({
-          message: error.message,
-        })
-      }
-
-      console.log(error)
-      return serverError()
+    constructor(updateUserUseCase) {
+        this.updateUserUseCase = updateUserUseCase
     }
-  }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const isIdValid = checkIfIdIsValid(userId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            await updateUserSquema.parseAsync(params)
+
+            const updatedUser = await this.updateUserUseCase.execute(
+                userId,
+                params,
+            )
+
+            return ok(updatedUser)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({
+                    message: error.message,
+                })
+            }
+
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.log(error)
+            return serverError()
+        }
+    }
 }

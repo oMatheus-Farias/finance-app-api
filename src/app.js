78797e914809c160ff7863b6c5656e04a084ccd4
Matheus <matheusfariasdasneves@gmail.com>
2024-04-27 11:07:53 -0300

    test: ensure GET /api/users/:userId return 200 when user is found

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+import { usersRouter, transactionsRouter } from './routes/index.js'
+
+export const app = express()
+
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
+app.use('/api/transactions', transactionsRouter)
--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,7 +1,6 @@
 import { faker } from '@faker-js/faker'
 import request from 'supertest'
-import { app } from '../..'
-
+import { app } from '../app.js'
 describe('User Routes E2E Tests', () => {
     it('POST /api/users should return 201 when user is created', async () => {
         const response = await request(app)
@@ -16,5 +15,25 @@ describe('User Routes E2E Tests', () => {
             })
 
         expect(response.status).toBe(201)
+    })
+
+    it('GET /api/users/:userId should return 200 when user is found', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            })
+
+        const result = await request(app).get(`/api/users/${createdUser.id}`)
+
+        expect(result.status).toBe(200)
+        expect(result.body.first_name).toBe(createdUser.first_name)
+        expect(result.body.last_name).toBe(createdUser.last_name)
+        expect(result.body.email).toBe(createdUser.email)
     })
 })

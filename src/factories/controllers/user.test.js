a87e1ef5c2a40310482f2a60f0e79bbc10988c25
Matheus <matheusfariasdasneves@gmail.com>
2024-04-27 09:21:01 -0300

    test: ensure makeDeleteUserController returns valid DeleteUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,8 +1,12 @@
-import { UpdateUserController } from '../../controllers'
-import { makeUpdateUserController } from './user'
+import { DeleteUserController, UpdateUserController } from '../../controllers'
+import { makeDeleteUserController, makeUpdateUserController } from './user'
 
 describe('User Controllers Factories', () => {
     it('should return a valid UpdateUserController instance', () => {
         expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
+    })
+
+    it('should return a valid DeleteUserController instance', () => {
+        expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
     })
 })
